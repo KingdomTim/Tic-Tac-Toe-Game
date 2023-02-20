2b6dfd7af98cdde4f631f6b88c28e70a26fa851d
Timothy Yakhya <yakhya.timothy@gmail.com>
2023-02-20 04:36:19 -0500

    addedFullUIFunctionalityAndResetOptions

--- main.js
+++ main.js
@@ -18,6 +18,8 @@ const gameBoard = (() => {
         for(let i = 0; i < board.length; i++) {
             board[i] = ''
         }
+        
+        console.log(getBoard())
     }
 
     return {getBoard, setCell, reset}
@@ -28,6 +30,8 @@ const gameController = (() => {
 
     const player1 = Player('Player 1','X')
     const player2 = Player('Player 2','O')
+    
+    let winner = null
 
     let currentTurn = player1.name
     let currentSymbol = player1.symbol
@@ -55,11 +59,12 @@ const gameController = (() => {
         let position = (Number(x.getAttribute('id').split('').pop()))
         let spot = gameBoard.getBoard()[position]
 
-    if(spot === '') {
+    if(spot === '' && winner === null) {
         gameBoard.setCell(position, currentSymbol)
+        switchTurn()
+        displayController.turnDisplay()
         console.log(gameBoard.getBoard())
         checkWin()
-        switchTurn()
         }
     }
 
@@ -74,24 +79,35 @@ const gameController = (() => {
         [2,4,6]
     ]
 
-    let winner = null
+    const getWinner = () => {
+        return winner
+    }
 
     const checkWin = () => {
 
         let array = gameBoard.getBoard()
-    
+
         if(winningCombinations.some((combination) =>
             combination.every((x) => array[x] === 'X' ) )) {
                 console.log('Player 1 Wins')
-                return winner = player1.name
+                winner = player1.name
             } else if (winningCombinations.some((combination) =>
             combination.every((x) => array[x] === 'O' ))) {
                 console.log('Player 2 Wins')
-                return winner = player2.name
+                winner = player2.name
+            } else if (winner === null && array.filter((x) => x === '').length === 0) {
+                winner = 'Tie'
             }
     }
 
-    return {getTurn, getSymbol, switchTurn, playRound, checkWin}
+    const resetGame = () => {
+        winner = null
+        currentTurn = player1.name
+        currentSymbol = player1.symbol
+
+    }
+
+    return {getTurn, getSymbol, getWinner, switchTurn, playRound, checkWin, resetGame}
 
 })()
 
@@ -101,20 +117,72 @@ const displayController = (() => {
     const announce = document.createElement('p')
     container.appendChild(announce)
 
-    boxes.forEach((box) => {box.addEventListener('click', function play() {
-        updateDisplay(box)
-        gameController.playRound(box)
-        this.removeEventListener('click', play) 
-    })})
 
-    const updateDisplay = (box) => {
+    const start = () => {
+
+    boxes.forEach((box) => {box.addEventListener('click', function play() {
+        symbolDisplay(box)
+        gameController.playRound(box)
+        winDisplay(box)
+        this.removeEventListener('click', play) 
+    })})}
+
+    const symbolDisplay = (box) => {
+
+        
+        if(box.textContent === '' && gameController.getWinner() === null) {
         box.textContent = gameController.getSymbol()
+    }}
+
+    const turnDisplay = () => {
+
         announce.textContent = `It is ${gameController.getTurn()}'s turn`
     }
 
+     const winDisplay = (box) => {
+
+
+        if(gameController.getWinner() === 'Tie' && box.textContent !== '') {
+            announce.textContent = `It is a ${gameController.getWinner()}.
+            Would you like to play again?`
+
+            const restart = document.createElement('button')
+            restart.textContent = 'Restart'
+            restart.classList.add = ('.restart')
+            container.appendChild(restart)
+            restart.addEventListener('click', () => {
+                restart.remove()
+                gameBoard.reset()
+                gameController.resetGame()
+                boxes.forEach((box) => {box.textContent = ''})
+                start()
+                turnDisplay()
+                
+                
+            })
+        } else if(gameController.getWinner() !== null && box.textContent !== '') {
+            announce.textContent = `${gameController.getWinner()} has won!
+            Would you like to play again?`
+            
+            const restart = document.createElement('button')
+            restart.textContent = 'Restart'
+            restart.classList.add = ('.restart')
+            container.appendChild(restart)
+            restart.addEventListener('click', () => {
+                restart.remove()
+                gameBoard.reset()
+                gameController.resetGame()
+                boxes.forEach((box) => {box.textContent = ''})
+                start()
+                turnDisplay()
+            })
+        }
+    }
+
+    start()
+    turnDisplay()
 
 
 
-
-    return {updateDisplay}
+    return {symbolDisplay, turnDisplay, winDisplay}
 })()
